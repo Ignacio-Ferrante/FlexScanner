533673bef4c51709bc10d653aa9c14779db60b91
Ignacio-Ferrante <ignacioferrante1379@gmail.com>
2020-11-30 23:31:15 -0300

    Mejoras de lo de patri

--- main.c
+++ main.c
@@ -1,35 +1,53 @@
+//////////////////////////////////////////////////////////////////////////////////////
+/*
+      TP3 - 2020
+      "Un escaner elemental"
+
+      INTEGRANTES: 
+      Alejandro Buergo / 168.569-7
+      Ignacio Ferrante / 171.524-0
+      Patricio Galli   / 172.467-8
+                                                                                          */
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokens.h"
+
 //char *token_names[] = {"", "Identificador\t\t", "Constante\t\t", "Asignación", "identificador inválido: ", "constante inválida: ", "cadena desconocida: ","Fin de archivo"};
 int main() {
+
 	enum token t;
+
 	while ((t = yylex()) != FDT){
 		switch(t){
-			case PALABRA_RESERVADA:
-			printf("Token: %s", toupper(yytext));
-			break;
 			case CARACTER_VALIDO:
-			printf("Token: '%c'", yytext[0]);
-			break;
-			case CONSTANTE:
-			printf("Token: Constante\t\t%slexema: %s\n", yytext);
-			break;
-			case IDENTIFICADOR:
-			printf("Token: Identificador\t\t%slexema: %s\n", yytext);
-			break;
+						printf("Token: '%c'", yytext[0]);
+						break;
+			case PALABRA_RESERVADA:
+						printf("Token: %s", toupper(yytext));
+						break;
 			case ASIGNACION:
-			printf("Token: Asignación");
-			break;
+						printf("Token: Asignación");
+						break;
+			case IDENTIFICADOR:
+						printf("Token: Identificador\t\t%slexema: %s\n", yytext);
+						break;
+			case CONSTANTE:
+						printf("Token: Constante\t\t%slexema: %s\n", yytext);
+						break;
 			case IDENTIFICADOR_INVALIDO:
-			printf("Error léxico: identificador inválido: %s", yytext);
-			break;
+						printf("Error léxico: identificador inválido: %s", yytext);
+						break;
 			case CONSTANTE_INVALIDA:
-			printf("Error léxico: constante inválida: %s", yytext);
-			break;
+						printf("Error léxico: constante inválida: %s", yytext);
+						break;
 			case CADENA_DESCONOCIDA:
-			printf("Error léxico: cadena desconocida: %s", yytext);
-			break;
+						printf("Error léxico: cadena desconocida: %s", yytext);
+						break;
 			default:
-			printf("Fin de archivo");
-			return 0;
+						printf("Fin de archivo");
+						return 0;
 		}
 	}
 }
