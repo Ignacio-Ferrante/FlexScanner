41389ab3f0a146573f7a14d6edd772004c483be0
Alejandro <62315321+abuergo@users.noreply.github.com>
2021-01-31 08:12:26 -0300

    Corrijo errores TP3

--- main.c
+++ main.c
@@ -15,18 +15,24 @@
 #include "scanner.h"
 #include "tokens.h"
 
-//char *token_names[] = {"", "Identificador\t\t", "Constante\t\t", "Asignación", "identificador inválido: ", "constante inválida: ", "cadena desconocida: ","Fin de archivo"};
 int main() {
-
 	TOKEN t;
-
-	while ((t = yylex()) != FDT){
-		switch(t){
-			case CARACTER_VALIDO:
-						printf("Token: '%c'\n", yytext[0]);
+	while ((t = yylex()) != FDT){					
+			switch(t){
+			case PROGRAMA: 
+						printf("Token: %s\n",yytext);
+						break;						
+			case FIN_PROG: 
+						printf("Token: %s\n",yytext);
 						break;
-			case PALABRA_RESERVADA:
-						printf("Token: palabra reservada %s\n", yytext);
+			case LEER: 
+						printf("Token: %s\n",yytext);
+						break;
+			case ESCRIBIR: 
+						printf("Token: %s\n",yytext);
+						break;
+			case DECLARAR: 
+						printf("Token: %s\n",yytext);
 						break;
 			case ASIGNACION:
 						printf("Token: Asignación\n");
@@ -37,17 +43,9 @@ int main() {
 			case CONSTANTE:
 						printf("Token: Constante\t\tlexema: %s\n", yytext);
 						break;
-			case IDENTIFICADOR_INVALIDO:
-						printf("Error léxico: identificador inválido: %s\n", yytext);
-						break;
-			case CONSTANTE_INVALIDA:
-						printf("Error léxico: constante inválida: %s\n", yytext);
-						break;
-			case CADENA_DESCONOCIDA:
-						printf("Error léxico: cadena desconocida: %s\n", yytext);
-						break;
 			default:
-						return 0;
+						printf("Token: '%c'\n", t);	
+						break;
 		}
-	} 		printf("Token: Fin de archivo \n"); // Esta re mal esto jaja, mañana lo reviso bien. Esto es para que a lo ultimo aparezca el fin de archivo
+	} 		printf("Token: Fin de archivo \n"); 
 }
